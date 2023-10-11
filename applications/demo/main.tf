af777d3f7a2350b84ac2d3a4fbdc2b510765d57e
bociankruk <bociankruk@gmail.com>
2023-10-11 21:06:36 +0200

    Split between two files

--- applications/demo/main.tf
+++ applications/demo/main.tf
@@ -1,16 +1,3 @@
-# The configuration for the `remote` backend.
-terraform {
-  backend "remote" {
-    # The name of your Terraform Cloud organization.
-    organization = "bociankruk-org"
-
-    # The name of the Terraform Cloud workspace to store Terraform state files in.
-    workspaces {
-      name = "terraform-digitalocean-demo"
-    }
-  }
-}
-
 # An example resource that does nothing.
 resource "null_resource" "example" {
   triggers = {
