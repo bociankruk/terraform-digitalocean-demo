b310e6f18e0f1aa52a7510ce9f68ffd304a06cb5
bociankruk <bociankruk@gmail.com>
2023-10-13 16:25:43 +0200

    Add angular app example (#5)
    * Add multiple user_data
    
    * Deploy angular app
    
    * Small refactor plus comments
    
    * Disable app, ready for demo

--- modules/terraform-digitalocean-app/monitoring.tf
+++ modules/terraform-digitalocean-app/monitoring.tf
@@ -1,11 +1,11 @@
 resource "digitalocean_monitor_alert" "cpu_alert" {
   alerts {
-    email = [var.monitoring_email]
+    email = [var.monitor_email]
   }
   window      = "5m"
   type        = "v1/insights/droplet/cpu"
   compare     = "GreaterThan"
-  value       = var.monitoring_cpu_threshold
+  value       = var.monitor_cpu_thr
   enabled     = true
   tags        = [var.app_name]
   description = "${var.app_name}-high-cpu-usage"
@@ -13,12 +13,12 @@ resource "digitalocean_monitor_alert" "cpu_alert" {
 
 resource "digitalocean_monitor_alert" "ram_alert" {
   alerts {
-    email = [var.monitoring_email]
+    email = [var.monitor_email]
   }
   window      = "5m"
   type        = "v1/insights/droplet/memory_utilization_percent"
   compare     = "GreaterThan"
-  value       = var.monitoring_ram_threshold
+  value       = var.monitor_ram_thr
   enabled     = true
   tags        = [var.app_name]
   description = "${var.app_name}-high-ram-usage"
@@ -26,12 +26,12 @@ resource "digitalocean_monitor_alert" "ram_alert" {
 
 resource "digitalocean_monitor_alert" "disk_alert" {
   alerts {
-    email = [var.monitoring_email]
+    email = [var.monitor_email]
   }
   window      = "5m"
   type        = "v1/insights/droplet/disk_utilization_percent"
   compare     = "GreaterThan"
-  value       = var.monitoring_disk_threshold
+  value       = var.monitor_disk_thr
   enabled     = true
   tags        = [var.app_name]
   description = "${var.app_name}-high-disk-usage"
