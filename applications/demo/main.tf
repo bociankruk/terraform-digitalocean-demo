e08d223a1fa0c2b15d7f899d9cd1d08ae15012d2
bociankruki <bociankruk@gmail.com>
2023-10-13 14:32:49 +0200

    Add multiple user_data

--- applications/demo/main.tf
+++ applications/demo/main.tf
@@ -2,14 +2,10 @@ resource "digitalocean_domain" "domain" {
   name = var.app_domain
 }
 
-data "local_file" "user_data" {
-  filename = "${path.module}/files/install-laravel.sh"
-}
-
 module "laravel-app" {
   source            = "../../modules/terraform-digitalocean-app"
   droplet_count     = 1
-  droplet_user_data = file("files/user_data.yaml")
+  droplet_user_data = file("files/laravel_user_data.yaml")
   region            = var.region
   app_name          = "laravel"
   app_domain        = var.app_domain
