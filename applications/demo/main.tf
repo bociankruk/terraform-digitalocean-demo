b7f336edd37b1cc0d903e3de6801879bb62c8a1d
bociankruk <bociankruk@gmail.com>
2023-10-14 20:54:24 +0200

    Deploy apps

--- applications/demo/main.tf
+++ applications/demo/main.tf
@@ -2,27 +2,27 @@ resource "digitalocean_domain" "domain" {
   name = var.app_domain
 }
 
-# module "laravel-app" {
-#   source            = "../../modules/terraform-digitalocean-app"
-#   droplet_count     = 1
-#   droplet_user_data = file("files/laravel_user_data.yaml")
-#   region            = var.region
-#   app_name          = "laravel"
-#   app_domain        = var.app_domain
-#   app_domain_id     = digitalocean_domain.domain.id
-#   app_http_port     = 8000
-#   monitoring_email  = "bociankruk@gmail.com"
-# }
+module "laravel-app" {
+  source            = "../../modules/terraform-digitalocean-app"
+  droplet_count     = 1
+  droplet_user_data = file("files/laravel_user_data.yaml")
+  region            = var.region
+  app_name          = "laravel"
+  app_domain        = var.app_domain
+  app_domain_id     = digitalocean_domain.domain.id
+  app_http_port     = 8000
+  monitoring_email  = "bociankruk@gmail.com"
+}
 
-# module "angular-app" {
-#   source            = "../../modules/terraform-digitalocean-app"
-#   droplet_count     = 1
-#   droplet_user_data = file("files/angular_user_data.yaml")
-#   region            = var.region
-#   app_name          = "angular"
-#   app_domain        = var.app_domain
-#   app_domain_id     = digitalocean_domain.domain.id
-#   app_http_port     = 4200
-#   monitor_email     = "bociankruk@gmail.com"
-#   monitor_cpu_thr   = 75
-# }
+module "angular-app" {
+  source            = "../../modules/terraform-digitalocean-app"
+  droplet_count     = 1
+  droplet_user_data = file("files/angular_user_data.yaml")
+  region            = var.region
+  app_name          = "angular"
+  app_domain        = var.app_domain
+  app_domain_id     = digitalocean_domain.domain.id
+  app_http_port     = 4200
+  monitor_email     = "bociankruk@gmail.com"
+  monitor_cpu_thr   = 75
+}
