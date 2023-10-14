a1774b64b59d1882c8006b1e05ff1331f5500d64
bociankruk <bociankruk@gmail.com>
2023-10-14 21:23:05 +0200

    Scale up to 2 instances per app and adjust alert rules (#8)

--- applications/demo/main.tf
+++ applications/demo/main.tf
@@ -4,7 +4,7 @@ resource "digitalocean_domain" "domain" {
 
 module "laravel-app" {
   source            = "../../modules/terraform-digitalocean-app"
-  droplet_count     = 1
+  droplet_count     = 2
   droplet_user_data = file("files/laravel_user_data.yaml")
   region            = var.region
   app_name          = "laravel"
@@ -12,12 +12,13 @@ module "laravel-app" {
   app_domain_id     = digitalocean_domain.domain.id
   app_http_port     = 8000
   monitor_email     = "bociankruk@gmail.com"
-  monitor_cpu_thr   = 75
+  monitor_cpu_thr   = 90
+  monitor_ram_thr   = 95
 }
 
 module "angular-app" {
   source            = "../../modules/terraform-digitalocean-app"
-  droplet_count     = 1
+  droplet_count     = 2
   droplet_user_data = file("files/angular_user_data.yaml")
   region            = var.region
   app_name          = "angular"
@@ -25,5 +26,5 @@ module "angular-app" {
   app_domain_id     = digitalocean_domain.domain.id
   app_http_port     = 4200
   monitor_email     = "bociankruk@gmail.com"
-  monitor_cpu_thr   = 75
+  monitor_cpu_thr   = 80
 }
